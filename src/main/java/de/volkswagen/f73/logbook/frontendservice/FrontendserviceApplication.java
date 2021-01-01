886e029fcd8a9bd57d873dd9f1fb668abd80a355
Mathias <mathias.kramer@gmail.com>
2021-01-01 13:43:43 +0100

    initial commit

---
+++ src/main/java/de/volkswagen/f73/logbook/frontendservice/FrontendserviceApplication.java
@@ -0,0 +1,13 @@
+package de.volkswagen.f73.logbook.frontendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendserviceApplication.class, args);
+	}
+
+}
